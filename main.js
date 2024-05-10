94d83aa51d1b8cc563deb4d48e727ceb49a1b6fe
jttest github <jamestogher567@gmail.com>
2024-05-09 20:11:01 -0400

    slight cleanup

--- main.js
+++ main.js
@@ -6,10 +6,6 @@ const numRows = 25;
 const numCols = 25;
 
 
-
-//const pair = { first: 4, second: 8 };
-//console.log(pair.second);
-
 // Get a reference to the grid container
 const gridContainer = document.getElementById("container");
 
@@ -66,7 +62,8 @@ for(let i = 0; i < numRows; i++) {
   for(let j = 0; j < numCols; j++) {
     document.getElementById(`square-${i}-${j}`).onclick = function() {
       if(canSetStart) {
-        this.style.backgroundColor = "yellow";
+        //this.style.backgroundColor = "yellow";
+        this.style.background = "radial-gradient(#F4F4F4, #FFB30F)";
         //startSqaure = this;
         startSqaure.first = i; startSqaure.second = j;
         canSetStart = false
@@ -85,52 +82,7 @@ for(let i = 0; i < numRows; i++) {
     }
   }
 }
-/*
-// Algo Start
-///////////////////////////////////
-function startAlgo(currentSquare, delay) {
-  let q = new Queue();
-  visited[currentSquare.first][currentSquare.second] = true;
-  q.enqueue(currentSquare);
-  console.log(q);
 
-  let n = numRows;
-  let m = numCols;
-
-  while(! q.isEmpty()) {
-    let row = q.front().first;
-    let col = q.front().second;
-
-    q.dequeue();
-
-    //Neighbors
-    for(let deltaR = -1; deltaR <= 1; deltaR++) {
-      for(let deltaC = -1; deltaC <= 1; deltaC++) {
-        if(Math.abs(deltaR) == Math.abs(deltaC)) {continue;}
-
-        let nRow = row + deltaR;
-        let nCol = col + deltaC;
-
-        if(nRow >= 0 && nRow < n &&
-          nCol >= 0 && nCol < m &&
-          !visited[nRow][nCol] ) {
-            visited[nRow][nCol] = true;
-            const squareId = `square-${nRow}-${nCol}`;
-            const square = document.getElementById(squareId);
-            square.style.backgroundColor = "green";
-
-
-            q.enqueue({first: nRow, second: nCol});
-          }
-      }
-    }
-    setTimeout(step, delay);
-  }
-  step()
-
-  console.log(n,m);
-}
-*/
 
 function startAlgo(currentSquare, delay) {
   let q = new Queue();
@@ -147,6 +99,10 @@ function startAlgo(currentSquare, delay) {
     let row = q.front().first;
     let col = q.front().second;
 
+    const neighborID = `square-${row}-${col}`;
+    const neighbor = document.getElementById(neighborID);
+    neighbor.style.background = "radial-gradient(#F4F4F4, #EC4E20)";
+
     q.dequeue();
 
     //Neighbors
@@ -163,7 +119,9 @@ function startAlgo(currentSquare, delay) {
             visited[nRow][nCol] = true;
             const squareId = `square-${nRow}-${nCol}`;
             const square = document.getElementById(squareId);
-            square.style.backgroundColor = "green";
+
+            //square.style.backgroundColor = "green";
+            square.style.background = "radial-gradient(#F4F4F4, #849324)"; //849324
 
 
             q.enqueue({first: nRow, second: nCol});
@@ -174,25 +132,5 @@ function startAlgo(currentSquare, delay) {
   }
   step()
 
-  console.log(n,m);
 }
 
-
-
-
-
-/*
-if canSetStart === false
-We are ready to statr the algo
-
-
-
-/*
-// Accessing individual squares
-const square_3_5 = document.getElementById("square-3-5");
-square_3_5.style.backgroundColor = "green";
-
-// You can also access squares using other methods, such as querySelector
-const square_6_7 = document.querySelector("#square-6-7");
-square_6_7.style.backgroundColor = "blue";
-*/
